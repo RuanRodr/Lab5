ddeb0825752d8c888afa3064ff9dd6b797eac53a
Ruan Rodrigues <141832407+RuanRodr@users.noreply.github.com>
2023-11-19 18:05:04 -0300

    Add files via upload

---
+++ Quest1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  if(argc != 4){
+    return 1;
+  }
+  
+  int num1 = atof(argv[1]);
+  int num2 = atof(argv[2]);
+  char letra = argv[3][0];
+
+  int resul;
+  
+  switch(letra){
+    case 's':
+    resul = num1 + num2;
+    break;
+    
+    case 'm':
+    resul = num1 * num2;
+    break;
+    
+    default:
+    puts("Operação incorreta");
+    break;
+  }
+  return 0;
+}
